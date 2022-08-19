ed201dd8217af071432c0b90470529ddbd69f7a9
MUHAMMET BATUHAN <109726859+mbatu02@users.noreply.github.com>
2022-08-19 14:04:10 +0300

    Add files via upload

---
+++ functionileparagraf.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Benim İsmim MUHAMMET BATUHAN</h2>
+
+<p id="demo">SOYADIM</p>
+
+<button type="button" onclick="myFunction()">Butona Basın</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "BİLGİN";
+}
+</script>
+
+</body>
+</html> 
