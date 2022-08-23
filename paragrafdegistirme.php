58e45cfcd1bc5125023a2d7775c66affebc50fe1
MUHAMMET BATUHAN <109726859+mbatu02@users.noreply.github.com>
2022-08-23 20:32:01 +0300

    Add files via upload
    paragraf değiştirme js.

---
+++ paragrafdegistirme.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>PARAĞRAF DEĞİŞTİRME</h2>
+
+<p id="demo">Adım Muhammet Soyadım İçin Tıklayınız</p>
+
+<button type="button" onclick="myFunction()">Tıkla</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "BİLGİN";
+}
+</script>
+
+</body>
+</html> 
