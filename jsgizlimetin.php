8e36e769dd5b2ca37859b2613c89ec6333d4b70c
MUHAMMET BATUHAN <109726859+mbatu02@users.noreply.github.com>
2022-08-24 12:52:20 +0300

    js ile gizli metin ekleme

---
+++ jsgizlimetin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavScript ile Gizli Metin</h2>
+
+<p>Muhammet Batuhan Bilgin. Yaşım</p>
+
+<p id="demo" style="display:none">25</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Tıkla</button>
+
+</body>
+</html> 
