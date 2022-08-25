023f3ca979f8a6b24197c6a26ac30fe6faf68af9
MUHAMMET BATUHAN <109726859+mbatu02@users.noreply.github.com>
2022-08-25 12:58:35 +0300

    JS ile paragraf değiştirme

---
+++ paragrafgizleme.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript ile metin gizleme</h2>
+
+<p id="demo">JAVA ÖGRENMEK ÇOK ZOR</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='none'">TIKLA!</button>
+
+</body>
+</html> 
